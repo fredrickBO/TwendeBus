3c7e7b66772da007e280cfa4ca28c66233fb547f
fredrickBO <fredrickoundobarasa@gmail.com>
2025-07-29 12:24:04 +0300

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twende_bus_ui/core/theme/app_theme.dart';
 import 'package:twende_bus_ui/features/auth/screens/auth_wrapper.dart';
+import 'package:twende_bus_ui/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ProviderScope(child: MyApp()));
 }
 
